da8b9048bd39898ce36f295cd0b5a5faa1c9b70b
Marina <marinasoon@gmail.com>
2024-01-13 15:57:38 +0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 13676;
+        int price = 20;
+        System.out.println(ticket/price);
+    }
 }
